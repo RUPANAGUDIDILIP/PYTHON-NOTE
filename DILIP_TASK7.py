f6619a5b9f4f9350bdf2afd9a2b777faa13de82a
RUPANAGUDIDILIP <129368570+RUPANAGUDIDILIP@users.noreply.github.com>
2023-04-27 15:11:44 +0530

    Add files via upload

---
+++ DILIP_TASK7.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+for i in x:
+    #i = [1,2,3,4]
+    for j in i:
+        
+        if j%3==0:
+            break
+        print(j,end = ' ')
+        
+        
+    print('hello')
+    print()
+
+
+# In[ ]:
+
+
+
+
